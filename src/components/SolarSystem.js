1db421d88928c96e8387f1158d71b9b1c278e034
Matheus Milanez <mattmila96@gmail.com>
2024-01-04 15:05:12 -0300

    final commit

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -9,9 +9,9 @@ class SolarSystem extends React.Component {
       <div data-testid="solar-system">
         <Title headline="Planetas" />
         {
-          planets.map((planeta, index) => (
+          planets.map((planeta) => (
             <PlanetCard
-              key={ index }
+              key={ planeta.name }
               planetName={ planeta.name }
               planetImage={ planeta.image }
             />
